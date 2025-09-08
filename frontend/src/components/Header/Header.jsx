f51e7e1a93ece88b4633ed1168f52f1f15eb27d7
Pouya <pouyabh1999@gmail.com>
2025-09-08 15:36:59 +0200

    header created

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import "./Header.css"
+
+function Header() {
+  return (
+    <div className="header">
+        <div className="header-contents">
+            <h2>Set Your Order</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit cum illum tempora nam id quo reiciendis voluptas quam exercitationem, animi quaerat aliquid recusandae veniam quibusdam quasi voluptatum. Non, dolor. Architecto exercitationem magni autem ut aspernatur iusto itaque iure quam ad inventore illum maiores deleniti a nemo veniam error, in amet?</p>
+            <button>View Menu</button>
+        </div>
+    </div>
+  )
+}
+
+export default Header
