049b82be0972cf1303ef2a5acb8d64f8637a821c
Pouya <pouyabh1999@gmail.com>
2025-09-18 13:51:33 +0200

    connectiong db via env

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
 import express from "express"
 import cors from "cors"
+import { connect } from "mongoose"
+import { connectDB } from "./config/db.js"
+import dotenv from "dotenv";
 
+//connecting for the .env file to the server
+dotenv.config();
 
 //app config
-
 const app = express()
 const port = 4000
 
@@ -11,8 +15,12 @@ const port = 4000
 
 app.use(express.json())
 
+//db connection for the connection in the import shuld always write db.js not db alone
+connectDB()
+
 //accessing backend from frontend using cors
 app.use( cors() )
+
 
 app.get("/" , ( req , res )=>{
     res.send("hello world")
