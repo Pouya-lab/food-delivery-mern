70f324f795771ce8005d2ff284e0829db535fefd
Pouya <pouyabh1999@gmail.com>
2025-10-16 11:42:53 +0200

    creating cart controller and router

---
+++ backend/controllers/cartControler.js
@@ -0,0 +1,18 @@
+import userModel from "../models/userModel.js";
+
+
+// add items to cart
+export const addToCart = async( req , res )=>{
+
+}
+
+
+// remove items from users cart
+export const removeFromCart = async( req , res ) =>{
+
+}
+
+// fetch user cart data
+export const getCart = async( req , res ) =>{
+
+}
