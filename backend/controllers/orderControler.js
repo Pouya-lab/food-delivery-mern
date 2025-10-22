468bc088355eed06fc042a35f23dbefb1699c948
Pouya <pouyabh1999@gmail.com>
2025-10-22 07:34:01 +0200

    initializing order files

---
+++ backend/controllers/orderControler.js
@@ -0,0 +1,7 @@
+import orderModel from "../models/orderModel.js";
+import userModle from "../models/userModel.js"
+ import stripe from "stripe";
+
+ export const placeOrder = async( req , res ) => {
+
+ }
--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import dotenv from "dotenv";
 import foodRouter from "./routes/foodRoute.js";
 import userRouter from "./routes/userRoute.js";
 import  cartRouter from "./routes/cartRoute.js";
+import orderRouter from "./routes/orderRoute.js";
 
 //connecting for the .env file to the server
 dotenv.config();
@@ -33,6 +34,7 @@ app.use("/api/food" , foodRouter)
 app.use("/images" , express.static('uploads'))
 app.use("/api/user" , userRouter)
 app.use("/api/cart" , cartRouter)
+app.use("/api/order" , orderRouter)
 
 
 app.get("/" , ( req , res )=>{
