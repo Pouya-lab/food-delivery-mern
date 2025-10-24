65ef96c149959bd677f54182ecb4131b805ec85c
Pouya <pouyabh1999@gmail.com>
2025-10-24 15:13:04 +0200

    getting users orders from DB

--- backend/controllers/orderControler.js
+++ backend/controllers/orderControler.js
@@ -76,3 +76,15 @@ export const verifyOrder = async( req , res )=>{
     
   }
 }
+
+// user orders for frontend
+export const userOrders = async( req , res )=>{
+  try {
+    const orders = await orderModel.find({ userId : req.userId })
+    res.json({ success : true , data : orders }) 
+  } catch (error) {
+    console.log(error);
+    res.json({ success : false , message : "Error" })
+    
+  }
+}
--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { placeOrder, verifyOrder } from "../controllers/orderControler.js";
+import { placeOrder, userOrders, verifyOrder } from "../controllers/orderControler.js";
 import authMiddleware from "../middleware/auth.js";
 
 const orderRouter = express.Router();
@@ -7,6 +7,8 @@ const orderRouter = express.Router();
 orderRouter.post( "/place" , authMiddleware , placeOrder )
 
 orderRouter.post("/verify" , verifyOrder )
+
+orderRouter.post("/userorders" , authMiddleware , userOrders );
 
 
 export default orderRouter;
